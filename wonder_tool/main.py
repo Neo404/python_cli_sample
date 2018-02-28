9e0ec041789ff0e012b1e4d29b4e6b799fe9d966
Neo404 <tell-tell-doll.a.fine.weather@docomo.ne.jp>
2018-02-28 20:51:48 +0900

    add

--- wonder_tool/main.py
+++ wonder_tool/main.py
@@ -1,5 +1,43 @@
 #-*- coding: utf-8 -*-
 import sys
+import urllib.parse
+import urllib.request
+import json
+import requests as rq
+
+def proofreading(check_message):
+    APIKEY = ""
+
+    url = "https://api.a3rt.recruit-tech.co.jp/proofreading/v2/typo?"
+
+    sentence=check_message
+
+    params = urllib.parse.urlencode(
+                {'apikey': APIKEY,
+                'sentence': sentence})
+                #params = "apikey=" + APIKEY + "&sentence=" + sentence
+    #response = urllib.request.urlopen(url+params)
+    response = rq.get(url+params)
+
+    data = json.loads(response.text)
+    return data
+    """
+    #print(url+params)
+    encontent = response.read()
+    #content = json.load(jsoncontent)
+    print(encontent["inputSentence"])
+    decontent = encontent.decode('utf-8')
+
+    return decontent
+    #print(response.read())"""
 
 def wonder_main(argv=sys.argv[1:]):
     print("What a wonder!")
+    if len(argv) == 1:
+        data = proofreading(argv)
+        print(data["inputSentence"])
+        #print(data["normalizedSentence"])
+        print(data["checkedSentence"])
+
+if __name__ == "__main__":
+    wonder_main()
